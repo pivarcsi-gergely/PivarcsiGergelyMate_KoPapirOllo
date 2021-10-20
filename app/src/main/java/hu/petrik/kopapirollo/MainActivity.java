82c3c9ac3c879daad777da57b1ca31f13c688875
Pivarcsi Gergely Mate <gergely.pivarcsi@gmail.com>
2021-10-20 18:11:09 +0200

    button funkciok, randomsorsolas

--- app/src/main/java/hu/petrik/kopapirollo/MainActivity.java
+++ app/src/main/java/hu/petrik/kopapirollo/MainActivity.java
@@ -3,14 +3,19 @@ package hu.petrik.kopapirollo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.IllegalFormatCodePointException;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView teKep, gepKep;
     private TextView emberScore, gepScore;
+    private int emberScoreSzam, gepScoreSzam, teTipp, gepTipp;
     private Button buttonKo, buttonPapir, buttonOllo;
 
 
@@ -18,9 +23,75 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
+        buttonKo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                teKep.setImageResource(R.drawable.rock);
+                gepSorsol();
+            }
+        });
+
+        buttonPapir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                teKep.setImageResource(R.drawable.paper);
+                gepSorsol();
+            }
+        });
+
+        buttonOllo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                teKep.setImageResource(R.drawable.scissors);
+                gepSorsol();
+            }
+        });
+
+    }
+
+    public void gepSorsol(){
+
+        if (gepTipp == 0){
+            gepKep.setImageResource(R.drawable.rock);
+        }
+        else if (gepTipp == 1){
+            gepKep.setImageResource(R.drawable.paper);
+        }
+        else{
+            gepKep.setImageResource(R.drawable.scissors);
+        }
+    }
+
+    public void kiNyerEgyFordulot(){
+        if(teTipp == 0 && gepTipp == 1){
+            gepScoreSzam++;
+            gepScore.setText("Computer" + gepScoreSzam);
+        }
+        else if(teTipp == 0 && gepTipp == 2){
+            emberScoreSzam++;
+        }
+        else if(teTipp == 1 && gepTipp == 0){
+            emberScoreSzam++;
+        }
+        else if(teTipp == 2 && gepTipp == 0){
+            gepScoreSzam++;
+        }
+        else if(teTipp == 2 && gepTipp == 1){
+            emberScoreSzam++;
+        }
+        else if(teTipp == 1 && gepTipp == 2){
+            gepScoreSzam++;
+        }
     }
 
     public void init(){
+        Random r = new Random();
+        teTipp = 0;
+        gepTipp = r.nextInt(3);
+        emberScoreSzam = 0;
+        gepScoreSzam = 0;
         teKep = findViewById(R.id.teKep);
         gepKep = findViewById(R.id.gepKep);
         emberScore = findViewById(R.id.emberScore);
